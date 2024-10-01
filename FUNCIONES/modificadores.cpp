f8d78d4d7a143dc15e38187f11e481a9c094bfae
Gustavo Alem Hernández Garcia <gustavo.hernandez35@my.unitec.edu.mx>
2024-09-30 21:30:12 -0600

    Update modificadores.cpp

--- FUNCIONES/modificadores.cpp
+++ FUNCIONES/modificadores.cpp
@@ -1,1 +1,28 @@
 
+// ConsoleApplication12.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+// funcion prototipo
+void imprime(vector<int> v);
+
+int main()
+{
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    imprime(v);
+}
+// Definiciones de la función
+void imprime(vector<int> v)
+{
+    for (auto elemento : v)
+    {
+        cout << elemento << " , ";
+    }
+    cout << endl;
+    system("pause");
+    system("cls");
+}
