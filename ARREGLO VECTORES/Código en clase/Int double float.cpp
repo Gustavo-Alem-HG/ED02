b035e7a3fbe7104592bcc4d7ed99518e9483d750
Gustavo Alem Hernández Garcia <gustavo.hernandez35@my.unitec.edu.mx>
2024-10-17 20:58:22 -0600

    Create Int double float.cpp

---
+++ ARREGLO VECTORES/Código en clase/Int double float.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Header.h"
+
+using namespace std;
+void suma(int a, int b);
+void suma(double a, double b);
+void suma(float a, float b);
+void suma(string a, string b);
+void suma(int a, double b);
+void suma(double a, int b);
+void suma(int a, float b);
+void suma(float a, int b);
+void suma(double a, float b);
+void suma(float a, double b);
+
+int main()
+{
+}
+// Definicion de la funcion 
+    void suma(int a, int b)
+    {
+        cout << a + b << endl;
+    }
+    void suma(double a, double b)
+    {
+        cout << a + b << endl;
+    }
+    void suma(float a, float b)
+    {
+        cout << a + b << endl;
+    }
+
